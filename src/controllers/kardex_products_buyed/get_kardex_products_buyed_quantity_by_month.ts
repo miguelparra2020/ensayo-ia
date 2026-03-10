f0de1f51cfa2dffe21f574e754336e41546787c4
miguelyanpo-dev <miguel.yanpo@gmail.com>
2026-03-10 15:34:57 -0500

    add-changes

--- src/controllers/kardex_products_buyed/get_kardex_products_buyed_quantity_by_month.ts
+++ src/controllers/kardex_products_buyed/get_kardex_products_buyed_quantity_by_month.ts
@@ -23,7 +23,7 @@ export const getKardexProductsBuyedQuantityByMonth = async (c: Context) => {
       WITH months AS (
         SELECT
           generate_series(
-            '2024-01-01'::date,
+            '2024-12-01'::date,
             DATE_TRUNC('month', CURRENT_DATE),
             INTERVAL '1 month'
           )::date AS month_start
